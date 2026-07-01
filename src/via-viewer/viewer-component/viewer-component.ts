d931b908b9833a9e5bba4d7127af40e14de2623a
Lucas Cavalcante <lucas@viaapia.com.br>
2026-07-01 16:52:49 -0300

    coisas simples

---
+++ src/via-viewer/viewer-component/viewer-component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-viewer-component',
+    standalone: true,
+    templateUrl: './viewer-component.html',
+    styleUrl: './viewer-component.css'
+})
+
+export class ViewerComponent{
+
+}
