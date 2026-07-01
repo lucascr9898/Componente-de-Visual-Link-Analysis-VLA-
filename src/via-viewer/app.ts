dcd4aea001b6128cc58b5487162d18b136c796aa
Lucas Cavalcante <lucas@viaapia.com.br>
2026-07-01 15:38:44 -0300

    inicialização do graph-view

--- src/via-viewer/app.ts
+++ src/via-viewer/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EntityTypeFilter } from './components/entity-type-filter/entity-type-filter';
 import { EntityList } from "./components/entity-list/entity-list";
+import { EntityDetails } from "./components/entity-details/entity-details";
+import { GraphView } from "./components/graph-view/graph-view";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EntityTypeFilter, EntityList],
+  imports: [RouterOutlet, EntityTypeFilter, EntityList, EntityDetails, GraphView],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/via-viewer/components/graph-view/graph-view.ts
@@ -0,0 +1,43 @@
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit} from "@angular/core";
+import { VlaNode } from "../../models/vla-node.model";
+import { VlaEdge } from "../../models/vla-edge.model";
+import { Network, DataSet } from "vis-network/standalone";
+
+@Component ({
+    selector: 'app-graph-view',
+    standalone: true,
+    templateUrl: './graph-view.html',
+    styleUrl : './graph-view.css',
+})
+
+export class GraphView implements AfterViewInit {
+    @Input() nodes: VlaNode[]=[];
+    @Input() edges: VlaEdge[]=[];
+    @Input() selectedNodeId: string | null = null;
+    @Output() nodeClicked = new EventEmitter<string>();
+
+    @ViewChild('graphContainer') graphContainer!: ElementRef;
+
+    ngAfterViewInit(): void {
+        console.log(this.graphContainer.nativeElement);
+    }
+
+    mapEdgesNetwork() {
+    return this.edges.map(edge => {
+        return {
+            id: edge.id,
+            from: edge.source,
+            to: edge.target,
+            label: edge.label,
+        };
+    });
+}
+
+mapNodesNetwork() {
+    return this.nodes.map(node => {
+        return {
+            id: node.id,
+            label: node.label,
+        };
+    });
+} }
